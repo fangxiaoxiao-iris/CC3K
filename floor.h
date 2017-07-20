e6378cfaf7c2df1e4d5fe7ba4a52318f76da0d06
方潇晓 <fangxiaoxiao@v1020-wn-24-54.campus-dynamic.uwaterloo.ca>
2017-07-20 17:53:36 -0400

    update floor.h

--- floor.h
+++ floor.h
@@ -1,25 +1,76 @@
 #ifndef __FLOOR_H__
 #define __FLOOR_H_
 
-#include "square.h"
+#include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <string>
+
+#include "grid.h"
+// include nearly all the .h files :(
 
 
 
 
 class Floor {
-	std::vector<std::vector<Square*>> theFloor;
+	std::vector<Square*> theFloor; // do not use arrays of arrays, use just one array 
+	                               // the length should be 79 * 25 
 	GameBoard* Board;
-	PlayerCharacter* PC;
+	PC* player;
 	int level;
+	
+	// manage the floor
+	
+	std::vector<Enemy*> en_arr; // 20 enemies on a floor 
+	std::vector<Potion*> po_arr; // 10 potions on a floor
+	std::vector<Gold*> gold_arr; // 10 golds on a floor
+	
+	// any need to record the 5 rooms?
+	
+	std::string fname; // optional command line argument
+	
 public:
+	Floor();
+	~Floor();
+	
+	bool GameOver();
+	bool GameWin();
+	void clearFloor();
+	
+	void init(); // initialize the floor without provided file
+	void init(std::string name); // initialize the floor with the file
+	void restart();
+	
+	// generate a series of things :(
+	
+	void make_player();
+	void make_stair();
+	void make_potion();
+	void make_gold();
+	void make_enemy();
+	
+	void pcMove();
+	void pcAtk();
+	
+	// use potion? pick up gold?
+	
+	
+	
+	
+	// mutators 
+	
+	void setFname(std::string name);
+	
+	
+	
+	// accessors 
+	
+	void getLevel();
+	void getScore();
+	
+	friend std::ostream &operator<<(std::ostream &out, const Floor *f);
 	
 	
 };
-
-
-
 
 #endif 
--- square.h
+++ square.h
@@ -12,8 +12,10 @@ class Square {
 	int row;
 	int col;
 	char symbol;
+	char prev; // in the case of first a dragon hoard, then PC walks over it 
 	vector<Square*> neighbors;
 	GameBoard* theBoard;
+	
 public:
 	Square(); // Default constructor
 	virtual ~Square();
@@ -25,10 +27,12 @@ public:
 	int get_row();
 	int get_col();
 	char get_sym();
+	char get_prev();
 	
 	// mutators
 	void setCoords(int r, int c); // Tells me my row and column number
 	void setBoard(GameBoard* gb);	
+	void setPrev(char c);
 };
 
 #endif 

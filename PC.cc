7ef605eb5574d2ceff0efbb1a6d06631b7d28d9f
黄靖惟 <huangjingwei@v1020-wn-119-52.campus-dynamic.uwaterloo.ca>
2017-07-21 14:56:41 -0400

    move() method added

--- PC.cc
+++ PC.cc
@@ -11,27 +11,61 @@ PC::PC(): Character() {
 void PC::use(Potion &) {
 }
 
+// change PC's coordinates and notify neighbors.
+static void proper_move(PC &p, int r_update, int c_update, int index) {
+	if (((p.neighbors[index]).get_sym() == '.') || 
+		((p.neighbors[index]).get_sym() == '#') || 
+		((p.neighbors[index]).get_sym() == '+')) {
+			p.setCoords(r_update, c_update);
+			p.notifyBoard();
+		} else {
+			cout << "Invalid direction. You are heading to a dead end." << endl;
+		}
+}
+
 void PC::move(std::string direction) override {
+	string type = gettype();
 	int r_coord = get_row();
 	int c_coord = get_col();
 	int r_update, c_update;
+	if (type == "Troll") {
+		int t_cur_hp = gethp();
+		int t_update_hp = t_cur_hp + 5;
+		sethp(t_update_hp);
+	}
+
 	if (direction == "no") {
 		r_update = r_coord;
-		setCoords(r_coord, c_coord+1);
+		c_update = c_coord-1;
+		proper_move(*this, r_update, c_update, 0);
 	} else if (direction == "so") {
-
+		r_update = r_coord;
+		c_update = c_coord+1;
+		proper_move(*this, r_update, c_update, 1);
 	} else if (direction == "ea") {
-
+		r_update = r_coord+1;
+		c_update = c_coord;
+		proper_move(*this, r_update, c_update, 2);
 	} else if (direction == "we") {
-
+		r_update = r_coord-1;
+		c_update = c_coord;
+		proper_move(*this, r_update, c_update, 3);
 	} else if (direction == "ne") {
-
+		r_update = r_coord-1;
+		c_update = c_coord+1;
+		proper_move(*this, r_update, c_update, 4);
 	} else if (direction == "nw") {
-
+		r_update = r_coord-1;
+		c_update = c_coord-1;
+		proper_move(*this, r_update, c_update, 5);
 	} else if (direction == "se") {
-
+		r_update = r_coord+1;
+		c_update = c_coord+1;
+		proper_move(*this, r_update, c_update, 6);
 	} else if (direction == "sw") {
-
+		r_update = r_coord+1;
+		c_update = c_coord-1;
+		proper_move(*this, r_update, c_update, 7);
 	} else {
 		cout << "Invalid direction. Please choose a valid direction from 
 		no (North), so (South), ea (East), we (West), ne (Northeast), 
@@ -40,7 +74,5 @@ void PC::move(std::string direction) override {
 }
 
 void PC::dead() override {
-	if (isDead()) {
-		notifyBoard();
-	}
+	notifyBoard();
 }

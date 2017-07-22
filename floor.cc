331c5f9a4ecd578f77aa9d60cb515ee0d2a2ff5e
方潇晓 <fangxiaoxiao@fangxiaoxiaodeMacBook-Air.local>
2017-07-22 18:35:10 -0400

    modified floor

--- floor.cc
+++ floor.cc
@@ -50,23 +50,113 @@ void clearFloor() {
 	gold_arr.clear();
 }
 
-void init_no_file(std::string name) {  // parameter name is always the default name
+void init_no_file(string name, string race) {  // parameter name is always the default name
 	
-	// where to add the level, in main.cc?
-	
-	
+	// record the HP of player in this game
+	int HP_record = this->player->gethp();
+		
 	this->clearFloor(); // clearFloor: remains player, level and fname
 
-	
+	// set fname field
+	this->faname = name;
+
+	// set level field
 	this->level = this->level + 1;
 	
-	//set Board field
+	// set theRoom field
+	// set the size of theRoom to 5
+	this->theRoom.reserve(5);
 	
+	// set the first room, this->theRoom[0].pos.emplace_back(k);
+	// row: 3-6 col: 3-28
+	for (int i = 3; i <= 6; i++) {
+		for (int j = 3; j <= 28; j++) {
+			pair<int, int> k = make_pair(i, j);
+			this->theRoom[0].pos.emplace_back(k);
+		}
+	}
+	
+	// set the second room, this->theRoom[1].pos.emplace_back(k);
+	// row: 15-21 col: 4-24
+	for (int i = 15; i <= 21; i++) {
+		for (int j = 4; j <= 24; j++) {
+			pair<int, int> k = make_pair(i, j);
+			this->theRoom[1].pos.emplace_back(k);
+		}
+	}
+	
+	// set the third room, this->theRoom[2].pos.emplace_back(k);
+	// row: 10-12 col: 38-49
+	for (int i = 10; i <= 12; i++) {
+		for (int j = 38; j <= 49; j++) {
+			pair<int, int> k = make_pair(i, j);
+			this->theRoom[2].pos.emplace_back(k);
+		}
+	}
+	
+	// set the forth room, this->theRoom[3].pos.emplace_back(k);
+	// divide into two rectangles and two random rows
+	
+	// first rectangle
+	// row: 3-6 col: 39-61
+	for (int i = 3; i <= 6; i++) {
+		for (int j = 39; j <= 61; j++) {
+			pair<int, int> k = make_pair(i, j);
+			this->theRoom[3].pos.emplace_back(k);
+		}
+	}
+	
+	// second rectangle
+	// row: 7-12 col: 61-75
+	for (int i = 7; i <= 12) {
+		for (int j = 61; j<= 75; j++) {
+			pair<int, int> k = make_pair(i, j);
+			this->theRoom[3].pos.emplace_back(k);
+		}
+	}
+	
+	// first random row
+	// row: 5 col: 62-69
+	for (int j = 62; j<= 69; j++) {
+		pair<int, int> k = make_pair(5, j);
+		this->theRoom[3].pos.emplace_back(k);
+	}
+	
+	// second random row
+	// row: 6 col: 62-72
+	for (int j = 62; j <= 72; j++) {
+		pair<int, int> k = make_pair(6, j);
+		this->theRoom[3].pos.emplace_back(k);
+	}
+	
+	// set the fifth room, this->theRoom[3].pos.emplace_back(k);
+	// divide into two rectangles
+	
+	// set the first rectangle
+	// row: 19-21 col: 37-75
+	for (int i = 19; i <= 21; i++) {
+		for (int j = 37; j <= 75; j++) {
+			pair<int, int> k = make_pair(i, j);
+			this->theRoom[4].pos.emplace_back(k);
+		}
+	}
+	
+	// set the second rectangle
+	// row: 16-21 col: 65-75
+	for (int i = 16; i<= 21; i++) {
+		for (int j = 65; j <= 75; j++) {
+			pair<int, int> k = make_pair(i, j);
+			this->theRoom[4].pos.emplace_back(k);
+		}
+	}
+	
+	// generate
+		
+	//set Board field
 	GameBoard* gb = new GameBoard(name); // give filename as parameter
 	this->Board = gb;
 	
 	// set theFloor field 
-	
 	ifstream file(name);
 	for (int i = 0; i < 25; i++) {
 		
@@ -88,7 +178,6 @@ void init_no_file(std::string name) {  // parameter name is always the default n
 	}
 	
 	// attach neighbors 
-	
 	for (int i = 0; i < 25; i++) {
 		for (int j = 0; j < 79; j++) {
 			if (i - 1 >= 0) {
@@ -125,23 +214,34 @@ void init_no_file(std::string name) {  // parameter name is always the default n
 				this->theFloor[i][j].attach("sw", &this->theFloor[i + 1][j - 1]);
 			}
 		}
-	}	
+	}
+	
+	// set player neighbors
+	this->player->setNeigh(theFloor[this->player->row][this->player->col].getNeigh());	
+	
+	// remain the HP of the player
+	this->player->sethp(HP_record);		
 }
 
 
-void Floor::init_file(std::string name) {
+void Floor::init_file(string name, string race) {
+	
+	// record the HP of player in this game
+	int HP_record = this->player->gethp();
 	
 	this->clearFloor();
 	
+	// set fname field
+	this->faname = name;
+	
+	// set the level field 
 	this->level = this->level + 1;
 	
 	//set Board field 
-	
 	GameBoard* gb = new GameBoard(name);
 	this->Board = gb;
 	
 	// set theFloor field
-	
 	ifstream file(name);
 	
 	for (int i = 0; i < 25; i++) {
@@ -156,6 +256,7 @@ void Floor::init_file(std::string name) {
 			// if we encounter special cases: num, letter
 			
 			if (c == '@') {  // player character 
+				/*
 				PC temp;
 				temp.settype("s");
 				temp.setCoords(i, j);
@@ -164,11 +265,89 @@ void Floor::init_file(std::string name) {
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);
+				*/
+				
+				if (race == "s") {
+					Shade temp;
+					
+					temp.settype("s");
+					temp.setCoords(i, j);
+					temp.setSym('@');
+					temp.setPrev('.');
+					temp.setBoard(gb);
+					
+					square_arr.emplace_back(temp);
+					
+					// set player field
+					PC* p = new Shade(i, j, '@', '.', gb);
+					this->player = p;
+					
+				} else if (race == "d") {
+					Drow temp;
+					
+					temp.settype("d");
+					temp.setCoords(i, j);
+					temp.setSym('@');
+					temp.setPrev('.');
+					temp.setBoard(gb);
+					
+					square_arr.emplace_back(temp);
+					
+					// set player field
+					PC* p = new Drow(i, j, '@', '.', gb);
+					this->player = p;
+					
+				} else if (race == "v") {
+					Vampire temp;
+					
+					temp.settype("v");
+					temp.setCoords(i, j);
+					temp.setSym('@');
+					temp.setPrev('.');
+					temp.setBoard(gb);
+					
+					square_arr.emplace_back(temp);
+					
+					// set player field
+					PC* p = new Vampire(i, j, '@', '.', gb);
+					this->player = p;
+					
+				} else if (race == "g") {
+					Goblin temp;
+					
+					temp.settype("g");
+					temp.setCoords(i, j);
+					temp.setSym('@');
+					temp.setPrev('.');
+					temp.setBoard(gb);
+					
+					square_arr.emplace_back(temp);
+					
+					// set player field
+					PC* p = new Goblin(i, j, '@', '.', gb);
+					this->player = p;
+					
+				} else if (race == "t") {
+					Troll temp;
+					
+					temp.settype("t");
+					temp.setCoords(i, j);
+					temp.setSym('@');
+					temp.setPrev('.');
+					temp.setBoard(gb);
+					
+					square_arr.emplace_back(temp);
+					
+					// set player field
+					PC* p = new Troll(i, j, '@', '.', gb);
+					this->player = p;
+					
+				}
 			} else if (c == 'H') {    // all types of enemies :(
 				Human temp;
 				temp.setCoords(i, j);
 				temp.setSym('H');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);
@@ -177,7 +356,7 @@ void Floor::init_file(std::string name) {
 				Dwarf temp;
 				temp.setCoords(i, j);
 				temp.setSym('W');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);
@@ -186,7 +365,7 @@ void Floor::init_file(std::string name) {
 				Elf temp;
 				temp.setCoords(i, j);
 				temp.setSym('E');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);
@@ -195,7 +374,7 @@ void Floor::init_file(std::string name) {
 				Orc temp;
 				temp.setCoords(i, j);
 				temp.setSym('O');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);
@@ -204,7 +383,7 @@ void Floor::init_file(std::string name) {
 				Merchant temp;
 				temp.setCoords(i, j);
 				temp.setSym('M');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);
@@ -213,7 +392,7 @@ void Floor::init_file(std::string name) {
 				Dragon temp;
 				temp.setCoords(i, j);
 				temp.setSym('D');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);
@@ -222,7 +401,7 @@ void Floor::init_file(std::string name) {
 				Halfling temp;
 				temp.setCoords(i, j);
 				temp.setSym('D');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);
@@ -232,7 +411,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('P');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setType("RH");
@@ -244,7 +423,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('P');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setType("BA");
@@ -256,7 +435,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('P');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setType("BD");
@@ -268,7 +447,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('P');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setType("PH");
@@ -280,7 +459,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('P');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setType("WA");
@@ -292,7 +471,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('P');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setType("WD");
@@ -304,7 +483,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('G');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setValue(2);
@@ -316,7 +495,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('G');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setValue(1);
@@ -328,7 +507,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('G');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setValue(4);
@@ -340,7 +519,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym('G');
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				temp.setValue(6);
@@ -352,7 +531,7 @@ void Floor::init_file(std::string name) {
 				
 				temp.setCoords(i, j);
 				temp.setSym(c);
-				temp.setPrev('i');
+				temp.setPrev('.');
 				temp.setBoard(gb);
 				
 				square_arr.emplace_back(temp);				
@@ -363,7 +542,6 @@ void Floor::init_file(std::string name) {
 	}
 	
 	// attach neighbors 
-	
 	for (int i = 0; i < 25; i++) {
 		for (int j = 0; j < 79; j++) {
 			if (i - 1 >= 0) {
@@ -400,7 +578,13 @@ void Floor::init_file(std::string name) {
 				this->theFloor[i][j].attach(&this->theFloor[i + 1][j - 1]);
 			}
 		}
-	}		
+	}
+	
+	// set player neighbors
+	this->player->setNeigh(theFloor[this->player->row][this->player->col].getNeigh());	
+	
+	// remain the HP of the player
+	this->player->sethp(HP_record);
 }
 
 
@@ -409,15 +593,52 @@ void clear();
 
 // generate a series of things :(
 
-void make_player();
+void make_player(string race) {
+	// generate coordinate
+	int room_num = rand() % 5;
+	int size = this->theRoom[room_num].pos.size();
+	
+	int index_num = rand() % size;
+	
+	int row = this->theRoom[room_num].pos[index_num].first;
+	int col = this->theRoom[room_num].pos[index_num].second;
+	
+	// change the corresponding square
+	
+	if (race == "s") {
+		Shade temp;
+		// this->theFloor[row][col] = temp;
+		// Do I need to change theFloor?
+		
+		temp.setSym('@');
+		temp.setCoords(row, col);
+		temp.setPrev('.');
+		temp.setBoard(this->Board);
+		
+		t
+	} else if (race == "d") {
+		Drow temp;
+		
+	}
+	
+	
+	
+	this->theFloor[row][col] 
+}
 void make_stair();
 void make_potion();
 void make_gold();
 void make_enemy();
 
 void pcMove(string direction) {
+	int old_r = this->player->get_row();
+	int old_c = this->player->get_col();
+	
+	
 	
 }
+
+
 void pcAtk(string direction) {
 	
 }
@@ -449,5 +670,4 @@ int getScore() {
 	}
 }
 
-friend std::ostream &operator<<(std::ostream &out, const Floor *f);
-
+friend std::ostream &operator<<(std::ostream &out, const Floor &f);

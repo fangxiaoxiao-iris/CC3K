9decac43a394b34b3595b81c12a4f85d4085ebaf
Ruitao Lai <Ruitao@Ruitaos-MacBook-Air.local>
2017-07-21 23:45:45 -0400

    update enemy move()

--- enemy.cc
+++ enemy.cc
@@ -3,40 +3,83 @@
 
 using namespace std;
 
-static void proper_random_ move(Enemy &e, int r_update, int c_update, int index) {
+static void proper_move(Enemy &e, int r_update, int c_update, int index) {
 	if((e.neighbors[index].get_sym() == '.')) {
 		e.setCoords(r_update, c_update);
+		p.swapNeighbors(e.neighbors[index_nb], index_nb, index_p);
 		e.notifyBoard();
-	} else {
-		cout << "Invalid direction. You are heading to a dead end." << endl;
 	}
 } 
+
+static bool isproper(Enemy &e, int index) {
+	if(e.neighbors[index].get_sym() == '.') {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 void Enemy::move(){
 	int r_coord = get_row();
 	int c_coord = get_col();
 	int r_update;
 	int c_update;
+	int random;
+	bool propermove;
+	bool stationary = true;
 
-	int random = rand % 8;
-
-	if(random == 0) {
-
-	} else if (random == 1) {
-
-	} else if (random == 2) {
-
-	} else if (random == 3) {
-
-	} else if (random == 4) {
-
-	} else if (random == 5) {
-
-	} else if (random == 6) {
-
-	} else {
-		// random == 7
+	for(int i = 0; i <= 7; ++i) {
+		if(this->neighbors[i].get_sym() == '.') {
+			stationary = false;
+		}
 	}
 
+	if(stationary == false) {
+	while(random = rand() % 8;) {
+		propermove = isproper(*this, random);
+		if(propermove == true) {
+			if(random == 0) {
+				r_update = r_coord;
+		        c_update = c_coord-1;
+				proper_move(*this, r_update, c_update, 0, 1);
+			} else if (random == 1) {
+				r_update = r_coord;
+				c_update = c_coord+1;
+				proper_move(*this, r_update, c_update, 1, 0);
+			} else if (random == 2) {
+					r_update = r_coord+1;
+					c_update = c_coord;
+					proper_move(*this, r_update, c_update, 2, 3);
+			} else if (random == 3) {
+					r_update = r_coord-1;
+					c_update = c_coord;
+					proper_move(*this, r_update, c_update, 3, 2);
+			} else if (random == 4) {
+					r_update = r_coord-1;
+					c_update = c_coord+1;
+					proper_move(*this, r_update, c_update, 4, 7);
+			} else if (random == 5) {
+					r_update = r_coord-1;
+					c_update = c_coord-1;
+					proper_move(*this, r_update, c_update, 5, 6);
+			} else if (random == 6) {
+					r_update = r_coord+1;
+					c_update = c_coord+1;
+					proper_move(*this, r_update, c_update, 6, 5);
+			} else {
+				// random == 7
+					r_update = r_coord+1;
+					c_update = c_coord-1;
+					proper_move(*this, r_update, c_update, 7, 4);
+			}
+		} else {
+		continue;
+}
+}
+}
+}
+
+void Enemy::dead() override {
+	notifyBoard();
 }
 

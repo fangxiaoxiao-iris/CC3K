ebfb4961dbdcd0271bae59c35033690dbc32ba62
Ruitao Lai <Ruitao@Ruitaos-MacBook-Air.local>
2017-07-22 23:15:19 -0400

    updated main.cc

--- main.cc
+++ main.cc
@@ -2,12 +2,33 @@
 #include <fstream>
 
 int main() {
+	string command;
+
 	if(argc == 1) {
 
 	} else {
 		// read in file
 	}
 
+while (cin >> command) {
+	if(command == "no" || command == "so" || command == "ea" || command == "we"
+		|| command == "ne" || command == "nw" || command == "se" || command == "sw") {
 
+	} else if (command == "u") {
+
+	} else if (command == "a") {
+
+	} else if (command == "s" || command == "d" || command == "v" || command == "g" || command == "t") {
+
+	} else if (command == "f") {
+
+	} else if (command == "r") {
+
+	} else if (command == "q") {
+
+	} else {
+		cout << "Invalid Command. Please try again." << endl;
+	}
+}
 
 }

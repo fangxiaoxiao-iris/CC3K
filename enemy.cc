29c66479d8cf02f1c4fe0b1b944c90b0a3cec2dc
Ruitao Lai <Ruitao@Ruitaos-MacBook-Air.local>
2017-07-22 20:53:46 -0400

    update enemy move

--- enemy.cc
+++ enemy.cc
@@ -10,9 +10,12 @@ Enemy::~Enemy() {}
 
 static void proper_move(Enemy &e, string index_nb, string index_p) {
 	if((e.neighbors[index_nb].get_sym() == '.')) {
-		e.setCoords(r_update, c_update);
-		p.swapNeighbors(e.neighbors[index_nb], index_nb, index_p);
-		e.notifyBoard();
+			e.swapCoords(p.neighbors[index_nb]);
+			char e_prev_sym = e.get_prev();
+			e.setPrev(e.neighbors[index_nb]);
+			(e.neighbors[index_nb]).setSym(e_prev_sym);
+			e.swapNeighbors(e.neighbors[index_nb], index_nb, index_p);
+			e.notifyMove(index_nb);
 	}
 } 
 
@@ -25,10 +28,7 @@ static bool isproper(Enemy &e, int index) {
 }
 
 void Enemy::move(){
-	int r_coord = get_row();
-	int c_coord = get_col();
-	int r_update;
-	int c_update;
+
 	int random;
 	bool propermove;
 	bool stationary = true;
@@ -44,37 +44,21 @@ void Enemy::move(){
 		propermove = isproper(*this, random);
 		if(propermove == true) {
 			if(random == 0) {
-				r_update = r_coord;
-		        c_update = c_coord-1;
 				proper_move(*this, "no", "so");
 			} else if (random == 1) {
-				r_update = r_coord;
-				c_update = c_coord+1;
 				proper_move(*this, "so", "no");
 			} else if (random == 2) {
-					r_update = r_coord+1;
-					c_update = c_coord;
 					proper_move(*this, "ea", "we");
 			} else if (random == 3) {
-					r_update = r_coord-1;
-					c_update = c_coord;
 					proper_move(*this, "we", "ea");
 			} else if (random == 4) {
-					r_update = r_coord-1;
-					c_update = c_coord+1;
 					proper_move(*this, "ne", "sw");
 			} else if (random == 5) {
-					r_update = r_coord-1;
-					c_update = c_coord-1;
 					proper_move(*this, "nw", "se");
 			} else if (random == 6) {
-					r_update = r_coord+1;
-					c_update = c_coord+1;
 					proper_move(*this, "se", "nw");
 			} else {
 				// random == 7
-					r_update = r_coord+1;
-					c_update = c_coord-1;
 					proper_move(*this, "sw", "ne");
 			}
 		} else {
